e5210bd44839901e6d9e0cc24c3c91738986ddc9
Grabli66 <grabli66@gmail.com>
2018-09-21 07:42:57 +0300

    Start TcpListener

--- src/core/io/TcpListener.hx
+++ src/core/io/TcpListener.hx
@@ -1,2 +1,41 @@
 package core.io;
 
+/**
+ * On client accept callback
+ */
+typedef OnAcceptCall = (Void) -> Void;
+
+/**
+ * Listens for connections from TCP network clients.
+ */
+class TcpListener {
+	/**
+	 * Host to listen
+	 */
+	public final host:String;
+
+	/**
+	 * Port to listen
+	 */
+	public final port:Int;
+
+    /**
+     * On client accept callback
+     */
+    public var onAccept : OnAcceptCall;
+
+	/**
+	 * Constructor
+	 */
+	public function new(host:String, port:Int) {
+		this.host = host;
+		this.port = port;
+	}
+
+    /**
+     * Open port on host and starts listen
+     */
+    public function open() : Void {
+
+    }
+}
--- src/core/time/DateTime.hx
+++ src/core/time/DateTime.hx
@@ -3,12 +3,29 @@ package core.time;
 /**
  * Datetime based on Float
  */
-abstract DateTime(Float) from Float to Float {
-    /**
-     * Constructor
-     * @param value 
-     */
-    public function new(value : Float) {
-        this = value;
-    }
+abstract DateTime(Float) {
+	/**
+	 * Get now local time
+	 * @return DateTime
+	 */
+	static public inline function now():DateTime {
+		return new DateTime(0);
+	}
+
+	/**
+	 * Return unix stamp representation of DateTime
+	 */
+	public var unixStamp(get, never):Float;
+
+	public function get_unixStamp():Float {
+		return 0;
+	}
+
+	/**
+	 * Constructor
+	 * @param value
+	 */
+	public function new(value:Float) {
+		this = value;
+	}
 }
