0f127ce7bc06c47b091fd0adcc8ae09975f315b3
Grabli66 <grabli66@gmail.com>
2018-09-27 14:36:03 +0300

    Comments

--- src/core/async/Waiter.hx
+++ src/core/async/Waiter.hx
@@ -1,11 +1,29 @@
 package core.async;
 
+/**
+ * Internal waiter struct
+ */
 typedef __Waiter = {
+    /**
+     * Call to wait
+     */
     var call: () -> Void;
+
+    /**
+     * Was waited to not hang
+     */
     var wasWaited:Bool;
 }
 
+/**
+ * Abstract on __Waiter
+ */
 abstract Waiter(__Waiter) from __Waiter to __Waiter {
+    /**
+     * Constructor
+     * @param call 
+     * @return ->Void)
+     */
     public function new(call:()->Void) {
         this = {
             call:call,
@@ -13,6 +31,9 @@ abstract Waiter(__Waiter) from __Waiter to __Waiter {
         };
     }
 
+    /**
+     * Start wait o return if was waited
+     */
     public function wait() {
         if (this.wasWaited)
             return;
