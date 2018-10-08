6d2354dcbb7135b2a9a6303330a5a90e686dbd15
Grabli66 <grabli66@gmail.com>
2018-10-08 11:36:50 +0300

    Improve byte type settings

--- src/core/io/port/ByteTypeSettings.hx
+++ src/core/io/port/ByteTypeSettings.hx
@@ -3,19 +3,48 @@ package core.io.port;
 /**
  * Settings for byte type of serial port
  */
-typedef ByteTypeSettings = {
-    /**
-     * Data bits: 6,7,8
-     */
-    var dataBits:Int;
+abstract ByteTypeSettings(ByteTypeSettingsImpl) from ByteTypeSettingsImpl to ByteTypeSettingsImpl {
+	/**
+	 * Constructor
+	 * @param dataBits
+	 * @param parity
+	 * @param stopBits
+	 */
+	public function new(dataBits:Int, parity:Parity, stopBits:Int) {
+		this = {
+			dataBits: dataBits,
+			parity: parity,
+			stopBits: stopBits
+		};
+	}
 
-    /**
-     * Parity: None, Even, Odd
-     */
-    var parity:Parity;
+	/**
+	 * Convert string to ByteTypeSettings
+	 * @param value
+	 * @return ByteTypeSettings
+	 */
+	@:from public static inline function fromString(value:String):ByteTypeSettings {
+		// TODO parse string
+		return new ByteTypeSettings(9600, Parity.None, 1);
+	}
+}
 
-    /**
-     * Stop bits: 2, 1.5, 1
-     */
-    var stopBits:Int;
+/**
+ * Settings for byte type of serial port data
+ */
+typedef ByteTypeSettingsImpl = {
+	/**
+	 * Data bits: 6,7,8
+	 */
+	var dataBits:Int;
+
+	/**
+	 * Parity: None, Even, Odd
+	 */
+	var parity:Parity;
+
+	/**
+	 * Stop bits: 2, 1.5, 1
+	 */
+	var stopBits:Int;
 }
